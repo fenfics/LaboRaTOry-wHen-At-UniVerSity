cd056596c3d43712c0327d7247aa979ad6fad87d
fxnzyGxrl. <93990784+fenfics@users.noreply.github.com>
2024-10-08 02:55:18 +0700

    Update MineSweeper.java

--- SoFtWaRe_ConStrucTioN/Lab11/MineSweeper.java
+++ SoFtWaRe_ConStrucTioN/Lab11/MineSweeper.java
@@ -4,109 +4,184 @@ import java.util.*;
 import javax.swing.*;
 
 public class MineSweeper extends JFrame {
-	private int row;
-	private int col;
-	private JButton[][] buttons;
-	private boolean[][] mines;
+    private int row;
+    private int col;
+    private JButton[][] buttons;
+    private boolean[][] mines;
+    private boolean[][] flags;
+    private int flagCount;
 
-	private void init(int i) {
-		mines = new boolean[row][col];
-		Random r = new Random();
-		while (i > 0) {
-			int ranX = r.nextInt(row - 1);
-			int ranY = r.nextInt(col - 1);
-			if (!mines[ranX][ranY]) {
-				mines[ranX][ranY] = true;
-				i--;
-			}
-		}
+    private void init(int i) {
+        mines = new boolean[row][col];
+        flags = new boolean[row][col];
+        flagCount = 0;
+        Random r = new Random();
+        while (i > 0) {
+            int ranX = r.nextInt(row);
+            int ranY = r.nextInt(col);
+            if (!mines[ranX][ranY]) {
+                mines[ranX][ranY] = true;
+                i--;
+            }
+        }
+    }
 
-		// mines[0][0] = true ;
-	}
+    private int count(int x, int y) {
+        int x1 = x - 1 < 0 ? 0 : x - 1;
+        int y1 = y - 1 < 0 ? 0 : y - 1;
+        int x2 = x + 2 > row ? row : x + 2;
+        int y2 = y + 2 > col ? col : y + 2;
+        int c = 0;
+        for (int i = x1; i < x2; i++) {
+            for (int j = y1; j < y2; j++) {
+                if (mines[i][j])
+                    c++;
+            }
+        }
+        return c;
+    }
 
-	private int count(int x, int y) {
-		int x1 = x - 1 < 0 ? 0 : x - 1;
-		int y1 = y - 1 < 0 ? 0 : y - 1;
-		int x2 = x + 2 > row - 1 ? row - 1 : x + 2;
-		int y2 = y + 2 > col - 1 ? col - 1 : y + 2;
-		System.out.println(x1 + " " + y1 + " " + x2 + " " + y2);
-		int c = 0;
-		for (int i = x1; i < x2; i++) {
-			for (int j = y1; j < y2; j++) {
-				if (mines[i][j])
-					c++;
-			}
-		}
-		return c;
-	}
+    private void reveal(int i, int j) {
+        if (i < 0 || j < 0 || i >= row || j >= col || buttons[i][j].getBackground() == Color.GRAY)
+            return;
 
-	private void reveal(int i, int j) {
+        int c = count(i, j);
+        buttons[i][j].setBackground(Color.GRAY);
+        buttons[i][j].setEnabled(false);
+        buttons[i][j].setText((c == 0 ? "" : c) + "");
+        buttons[i][j].setFont(new Font("Consolas", Font.BOLD, 30));
 
-		if (i == -1 || j == col || i == row || j == -1)
-			return;
+        if (c == 0) {
+            reveal(i - 1, j);
+            reveal(i, j + 1);
+            reveal(i + 1, j);
+            reveal(i, j - 1);
+        }
 
-		if (buttons[i][j].getBackground() == Color.GRAY)
-			return;
-		int c = count(i, j);
+        checkWin();
+    }
 
-		buttons[i][j].setBackground(Color.GRAY);
-		buttons[i][j].setEnabled(false);
-		buttons[i][j].setText((c == 0 ? "" : c) + "");
+    private void checkWin() {
+        int openedCells = 0;
+        int correctFlags = 0;
 
-		buttons[i][j].setFont(new Font("Consolas", Font.BOLD, 30));
-		if (c > 0)
-			return;
-		reveal(i - 1, j);
-		reveal(i, j + 1);
-		reveal(i + 1, j);
-		reveal(i, j - 1);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (buttons[i][j].getBackground() == Color.GRAY && !mines[i][j]) {
+                    openedCells++;
+                }
+                if (mines[i][j] && flags[i][j]) {
+                    correctFlags++;
+                }
+            }
+        }
 
-	}
+        if (openedCells + countTotalMines() == row * col) { 
+            JOptionPane.showMessageDialog(this, "You win!");
+            resetGame();
+        }
+    }
 
-	public MineSweeper(String title, int row, int col) {
-		super(title);
-		this.col = col;
-		this.row = row;
-		JPanel jPanel = new JPanel();
-		jPanel.setLayout(new GridLayout(row, col));
-		buttons = new JButton[row][col];
-		init(20);
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				buttons[i][j] = new JButton();
-				buttons[i][j].setBackground(Color.GREEN);
-				if (mines[i][j])
-					buttons[i][j].setText("B");
-					buttons[i][j].addActionListener(new ActionListener() {
+    private int countTotalMines() {
+        int totalMines = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (mines[i][j]) {
+                    totalMines++;
+                }
+            }
+        }
+        return totalMines;
+    }
 
-					public void actionPerformed(ActionEvent e) {
-						JButton b = (JButton) e.getSource();
-						for (int i = 0; i < row; i++) {
-							for (int j = 0; j < col; j++) {
-								if (b == buttons[i][j]) {
-									reveal(i, j);
-									return;
-								}
-								if (buttons[i][j].getText()=="B") {
-									JOptionPane.showMessageDialog(null, "You lose");
-									System.exit(0);
-								}
-							}
-						}
-					}
-				});
+    private void resetGame() {
+        dispose();
+        new MineSweeper("Mine Sweeper", row, col);
+    }
 
-				jPanel.add(buttons[i][j]);
-			}
-		}
-		add(jPanel);
-		setSize(500, 500);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
+    public MineSweeper(String title, int row, int col) {
+        super(title);
+        this.row = row;
+        this.col = col;
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new GridLayout(row, col));
+        buttons = new JButton[row][col];
+        init(20);
 
-	public static void main(String[] args) {
-		new MineSweeper("Mine Sweeper", 10, 10);
-	}
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                buttons[i][j] = new JButton();
+                buttons[i][j].setBackground(Color.GREEN);
 
+                if (mines[i][j]) {
+                    buttons[i][j].setText("B");
+                }
+
+                buttons[i][j].addMouseListener(new MouseAdapter() {
+                    public void mouseClicked(MouseEvent e) {
+                        JButton b = (JButton) e.getSource();
+                        for (int i = 0; i < row; i++) {
+                            for (int j = 0; j < col; j++) {
+                                if (b == buttons[i][j]) {
+                                    if (SwingUtilities.isRightMouseButton(e)) {
+                                        toggleFlag(i, j);
+                                    } else if (!flags[i][j]) {
+                                        if (mines[i][j]) {
+                                            buttons[i][j].setText("B");
+                                            buttons[i][j].setBackground(Color.RED);
+                                            JOptionPane.showMessageDialog(null, "You lose!");
+                                            showAllMines();
+                                            resetGame();
+                                        } else {
+                                            reveal(i, j);
+                                        }
+                                    }
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                });
+
+                jPanel.add(buttons[i][j]);
+            }
+        }
+
+        add(jPanel);
+        setSize(500, 500);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void toggleFlag(int i, int j) {
+        if (buttons[i][j].getBackground() == Color.GRAY) {
+            return;
+        }
+        if (flags[i][j]) {
+            flags[i][j] = false;
+            buttons[i][j].setText("");
+            buttons[i][j].setBackground(Color.GREEN);
+            flagCount--;
+        } else {
+            flags[i][j] = true;
+            buttons[i][j].setText("F");
+            buttons[i][j].setBackground(Color.YELLOW);
+            flagCount++;
+        }
+    }
+
+    private void showAllMines() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (mines[i][j]) {
+                    buttons[i][j].setText("B");
+                    buttons[i][j].setBackground(Color.RED);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new MineSweeper("Mine Sweeper", 10, 10);
+    }
 }
